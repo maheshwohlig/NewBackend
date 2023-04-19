310b96d36fd12e25b96c10ac3a95a35686e472df
mahesh <mahesh.jethwa@wohlig.com>
2023-04-19 11:20:24 +0530

    dynamic create member

--- my-app/pages/index.vue
+++ my-app/pages/index.vue
@@ -1,62 +1,99 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12">
-      <div>
-        <v-simple-table class="py-1 px-3 elevation-1">
-          <template v-slot:default>
-            <thead>
-              <tr class="grey lighten-25">
-                <th class="text-left">#</th>
-                <th class="text-left">UserName</th>
-                <th class="text-left">Currency</th>
-                <th class="text-left">Credit Limit</th>
-                <th class="text-left">Net Exposure</th>
-                <th class="text-left">Take</th>
-                <th class="text-left">Give</th>
-                <th class="text-left">Status</th>
-                <th class="text-left">Info</th>
-                <th class="text-left">Detail</th>
-                <th class="text-left">Action</th>
-                <th class="text-left">Lock</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>bunny</td>
-                <td>POINT@1</td>
-                <td>1,00,000</td>
-                <td>NA</td>
-                <td>0</td>
-                <td>0</td>
-                <td>Active</td>
-                <td>
-                  <v-icon>mdi-alpha-i-circle-outline</v-icon>
-                </td>
-                <td>
-                  <v-btn to="/memberEdit"><v-icon>mdi-pen</v-icon></v-btn>
-                </td>
-                <td>
-                  <v-btn to="/memberDetails"><v-icon>mdi-eye</v-icon></v-btn>
-                </td>
-                <td class="lockIcon d-flex align-center">
-                  <v-btn @click="showModal()"><v-icon>mdi-lock</v-icon></v-btn>
-                  <member-lists
-                    @clicked="onClickChild"
-                    :visible="isModalVisible"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+  <div>
+    <v-container>
+      <v-card>
+        <v-row class="m-0">
+          <v-col class="pt-4 pl-6" cols="12" sm="6" md="6">
+            <div>
+              <v-text-field
+                class="rounded-pill"
+                outlined
+                label="Search Member"
+                single-line
+                hide-details
+              ></v-text-field>
+            </div>
+          </v-col>
+          <v-col class="d-flex align-center">
+            <div>
+              <v-btn
+                @click="newMember('Create')"
+                color="error"
+                class="py-6 px-6 rounded-pill"
+              >
+                <v-icon>mdi-account-plus</v-icon>Create New</v-btn
+              >
+            </div></v-col
+          >
+        </v-row>
 
-        <v-flex xs12 md3 class="">
-          <v-pagination :length="15" :total-visible="7"></v-pagination>
-        </v-flex>
-      </div>
-    </v-col>
-  </v-row>
+        <v-row justify="center">
+          <v-col cols="12">
+            <div>
+              <v-simple-table class="py-1 px-3 elevation-1">
+                <template v-slot:default>
+                  <thead>
+                    <tr class="grey lighten-25">
+                      <th class="text-left">#</th>
+                      <th class="text-left">UserName</th>
+                      <th class="text-left">Currency</th>
+                      <th class="text-left">Credit Limit</th>
+                      <th class="text-left">Net Exposure</th>
+                      <th class="text-left">Take</th>
+                      <th class="text-left">Give</th>
+                      <th class="text-left">Status</th>
+                      <th class="text-left">Info</th>
+                      <th class="text-left">Detail</th>
+                      <th class="text-left">Action</th>
+                      <th class="text-left">Lock</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>bunny</td>
+                      <td>POINT@1</td>
+                      <td>1,00,000</td>
+                      <td>NA</td>
+                      <td>0</td>
+                      <td>0</td>
+                      <td>Active</td>
+                      <td>
+                        <v-icon>mdi-alpha-i-circle-outline</v-icon>
+                      </td>
+                      <td>
+                        <v-btn to="/memberDetails"
+                          ><v-icon>mdi-eye</v-icon></v-btn
+                        >
+                      </td>
+                      <td>
+                        <v-btn @click="newMember('Edit')"
+                          ><v-icon>mdi-pen</v-icon></v-btn
+                        >
+                      </td>
+                      <td class="lockIcon d-flex align-center">
+                        <v-btn @click="showModal()"
+                          ><v-icon>mdi-lock</v-icon></v-btn
+                        >
+                        <member-lists
+                          @clicked="onClickChild"
+                          :visible="isModalVisible"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+
+              <v-flex xs12 md3 class="">
+                <v-pagination :length="15" :total-visible="7"></v-pagination>
+              </v-flex>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -77,11 +114,22 @@ export default {
     onClickChild(value) {
       this.isModalVisible = value;
     },
+    newMember(data) {
+      this.$router.push({
+        path: `member/${data}`,
+        query: {
+          memberId: "13312312",
+        },
+      });
+    },
   },
 };
 </script>
 <style scoped>
 .lockIcon {
   padding: 0 !important;
+}
+.v-text-field--outlined >>> fieldset {
+  border-color: rgba(218, 118, 41, 0.986);
 }
 </style>
--- my-app/pages/memberDetails.vue
+++ my-app/pages/memberDetails.vue
@@ -1,28 +1,54 @@
 <template>
-  <v-card>
-    <v-tabs v-model="currentItem" hide-slider>
-      <v-tab
-        class="font-weight-bold white--text v-btn v-btn--depressed v-btn--flat v-btn--outlined theme--dark text--accent-4 mx-3 my-2 py-5 px-5"
-        v-for="item in items"
-        :key="item.title"
-        :value="'tab-' + item"
-      >
-        {{ item.title }}
-      </v-tab>
-    </v-tabs>
-
-    <v-window v-model="currentItem">
-      <v-window-item
-        :transition="false"
-        v-for="item in items"
-        :key="item.title"
-      >
-        <h2>{{ item.title }}</h2>
-        text
-        <component v-bind:is="item.link"></component>
-      </v-window-item>
-    </v-window>
-  </v-card>
+  <div>
+    <v-container>
+      <v-card class="px-10 py-8">
+        <div class="d-flex pl-5 mb-8">
+          <div class="d-flex">
+            <v-icon @click="goBack()">mdi-arrow-left-bold-circle</v-icon>
+          </div>
+          <div class="headline mx-3 d-flex align-center">
+            Member Listing Downline > xyz
+          </div>
+          <v-text-field
+            class="rounded-pill mx-5"
+            outlined
+            density="compact"
+            variant="solo"
+            label="Search Member"
+            append-inner-icon="mdi-magnify"
+            single-line
+            hide-details
+          ></v-text-field>
+          <div class="pr-5 d-flex align-center">
+            Created On: 1 Oct, 2022 22:31:46
+          </div>
+        </div>
+        <v-tabs v-model="currentItem" hide-slider>
+          <v-tab
+            class="font-weight-bold white--text v-btn v-btn--depressed v-btn--flat v-btn--outlined theme--dark text--accent-4 mx-3 my-2 py-5 px-5"
+            v-for="item in items"
+            :key="item.title"
+            :value="'tab-' + item"
+          >
+            {{ item.title }}
+          </v-tab>
+        </v-tabs>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-window v-model="currentItem">
+        <v-window-item
+          :transition="false"
+          v-for="item in items"
+          :key="item.title"
+        >
+          <h2>{{ item.title }}</h2>
+          text
+          <component v-bind:is="item.link"></component>
+        </v-window-item>
+      </v-window>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
@@ -40,10 +66,18 @@ export default {
       ],
     };
   },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 <style scoped>
 .btn-link a {
   text-decoration: none;
+}
+.v-text-field--outlined >>> fieldset {
+  border-color: rgba(218, 118, 41, 0.986);
 }
 </style>
