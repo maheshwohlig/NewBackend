81b3df629163e1d0c3eb36768515fe557e164432
mahesh <mahesh.jethwa@wohlig.com>
2023-04-21 10:18:40 +0530

    auth added

--- my-app/nuxt.config.js
+++ my-app/nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
 
-  modules: ["@nuxtjs/axios"],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
